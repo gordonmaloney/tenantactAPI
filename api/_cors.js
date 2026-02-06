f6a06fdff87262bb06ddcb57323aa2e3898566f8
Nate Wright <natew@notthisway.com>
2026-02-06 16:31:15 +0000

    Use .env to support local development and document setup

--- api/_cors.js
+++ api/_cors.js
@@ -2,9 +2,11 @@
 export function setCors(req, res) {
   const origin = req.headers.origin || "";
   // allow apex and any subdomain of tenantact.org (with optional port)
-  const allowed = /^https?:\/\/([a-z0-9-]+\.)*tenantact\.org(?::\d+)?$/i;
+  const allowed = /^https?:\/\/([a-z0-9-]+\.)*tenantact\.org(?::\d+)?$/i
 
-  if (allowed.test(origin)) {
+  if (process.env?.DISABLE_CORS === 'true') {
+    res.setHeader("Access-Control-Allow-Origin", '*');
+  } else if (allowed.test(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
   }
   res.setHeader("Vary", "Origin"); // important for caching
